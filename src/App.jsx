a451a1324e0a98de9316e41e18adeca50a85c63c
iamsarb001 <sarbbsandhu555@gmail.com>
2025-05-25 13:00:20 +0530

    external file -added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
 
 import './App.css'
+import { ListofStories } from './config/Data'
 
 function App() {
-
-  const ListofStories = [
-    { id : 1 , image : "https://www.shutterstock.com/image-photo/happy-young-man-sitting-home-260nw-2248472067.jpg" , name : " Amandeep " },
-    { id : 2 , image : "https://c.pxhere.com/photos/1e/73/portrait_man_man_portrait_male_person_adult_face_caucasian-601668.jpg!d" , name : " diljeet " },
-    { id : 3 , image : "https://thumbs.dreamstime.com/b/natural-looking-young-beautiful-woman-hazel-eyes-posing-fields-natural-looking-young-beautiful-woman-hazel-eyes-230310435.jpg" , name : " ranveer " },
-    { id : 4 , image : "https://png.pngtree.com/background/20231210/original/pngtree-making-stop-people-happy-single-photo-picture-image_6772295.jpg" , name : " Alia " },
-    { id : 5 , image : "https://images.unsplash.com/photo-1619950481763-a39e29f3b1e1?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fG1hbiUyMG5pZ2h0fGVufDB8fDB8fHww" , name : " vanveer " }
-  ]
 
   return (
     <div>
@@ -23,7 +16,7 @@ function App() {
                       <div className='w-[10vh] h-[10vh]'> 
                         <img src = {i?.image} alt = {i?.name} className = 'w-full h-full object-cover rounded-4xl '  /> 
                       </div>
-                   <div className = 'text-[10px]'> {i?.name} </div>
+                   <div className = 'flex justify-center text-[10px]'> {i?.name} </div>
                 </div>
               )
             })}
---
+++ src/config/Data.js
@@ -0,0 +1,13 @@
+
+
+export  const ListofStories = [
+    { id : 1 , image : "https://www.shutterstock.com/image-photo/happy-young-man-sitting-home-260nw-2248472067.jpg" , name : " Amandeep " },
+    { id : 2 , image : "https://c.pxhere.com/photos/1e/73/portrait_man_man_portrait_male_person_adult_face_caucasian-601668.jpg!d" , name : " diljeet " },
+    { id : 3 , image : "https://thumbs.dreamstime.com/b/natural-looking-young-beautiful-woman-hazel-eyes-posing-fields-natural-looking-young-beautiful-woman-hazel-eyes-230310435.jpg" , name : " ranveer " },
+    { id : 4 , image : "https://png.pngtree.com/background/20231210/original/pngtree-making-stop-people-happy-single-photo-picture-image_6772295.jpg" , name : " Alia " },
+    { id : 5 , image : "https://images.unsplash.com/photo-1619950481763-a39e29f3b1e1?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fG1hbiUyMG5pZ2h0fGVufDB8fDB8fHww" , name : " vanveer " },
+    { id : 6 , image : "https://www.shutterstock.com/image-photo/portrait-young-handsome-man-smiling-260nw-213159280.jpg" , name : " Sofia " },
+    { id : 7 , image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSPBtrSEuVEqpM1H5HYX4AFonivBf68pI6hVYGKcSTeZyAGYhVzz-G9Kqzc-W15Va80Rw&usqp=CAU" , name : " Usha " },
+    { id : 8 , image : "https://png.pngtree.com/background/20231210/original/pngtree-making-stop-people-happy-single-photo-picture-image_6772295.jpg" , name : " Manveer " },
+    { id : 9 , image : "https://www.shutterstock.com/image-photo/indian-man-260nw-334463564.jpg" , name : " Tanudeep " }
+  ]
