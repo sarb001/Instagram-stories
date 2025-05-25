14f01ba55006e589d50789e8847e4b8c3aeff15a
iamsarb001 <sarbbsandhu555@gmail.com>
2025-05-25 14:25:16 +0530

    showing openstory -

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
 
+import { useState } from 'react'
 import './App.css'
 import { ListofStories } from './config/Data'
+import { MdCancel } from 'react-icons/md';
 
 function App() {
 
+   const[openfullstory,setopenfullstory] = useState(false);
+   const[Allfullstory,setAllfullstory] = useState([
+   ]);
+
+  const Storyshowhandler = (mainimage,name) => {   
+          setopenfullstory(true);
+          const NewStory = { image : mainimage, name : name }
+          setAllfullstory((prev) => [
+             ...prev,
+             NewStory
+          ]);
+
+          console.log('al story =',Allfullstory);
+  }
+
   return (
     <div> 
-      <div className='md:hidden'>
+      <div className='md:hidden cursor-pointer'>
 
          <div className=' flex w-full overflow-x-scroll '>
-            {ListofStories?.map(i => {
+            {ListofStories?.map((i,index) => {
               return (
-                <div key = {i?.id} className='my-4 mx-2'>
+                <div key = {index} className='my-4 mx-2' onClick={() => Storyshowhandler(i?.image,i?.name)} >
                       <div className='w-[10vh] h-[10vh]'> 
-                        <img src = {i?.image} alt = {i?.name} className = 'w-full h-full object-cover rounded-4xl '  /> 
+                        <img src = {i?.image} alt = {i?.name} className = ' w-full h-full object-cover rounded-4xl '  /> 
                       </div>
                    <div className = 'flex justify-center text-[10px]'> {i?.name} </div>
                 </div>
@@ -21,7 +38,22 @@ function App() {
             })}
          </div>
 
-          <div className='grid justify-center  items-center h-[75vh] font-sans italic' >
+            <div>
+              {Allfullstory && Allfullstory?.map(i => {
+                return (
+                  <div key = {i?.id}>
+                        <div className='w-full z-50 h-[100vh] p-6'> 
+                          <span className='absolute z-[100]  top-30 bg-gray-500 p-[1px] rounded-full '> 
+                            <MdCancel className='text-3xl' /> 
+                          </span>
+                          <img src = {i?.image} alt = {i?.name} className=' relative  w-full h-full object-cover' /> 
+                        </div>
+                  </div>
+                )
+              })}
+            </div>
+
+          <div className='grid -z-10 justify-center  items-center h-[75vh] font-sans italic' >
             <div>
               <div> Instagram Stories </div>
               <div> For Mobile View only </div>
